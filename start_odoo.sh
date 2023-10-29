6d3bd29d616d324dc46a6d81de7a17cf6262375c
BahirHakimy <bahirhakimy2020@gmail.com>
2023-10-29 13:09:49 +0430

    Add the portal

--- start_odoo.sh
+++ start_odoo.sh
@@ -3,11 +3,12 @@
 # Configuration
 ODOO_BIN="/home/bhakimi/workspace/source/community/v 16.0/odoo/odoo-bin"
 ADDONS_PATH="/home/bhakimi/workspace/source/community/v 16.0/odoo/addons"
-ENTERPRISE_ADDONS="/home/bhakimi/workspace/source/enterprise/v 16.0/odoo/addons"
-CUSTOM_ADDONS="/home/bhakimi/workspace/source/community/v 16.0/my_modules,/home/bhakimi/workspace/source/community/v 16.0/myaddons"
-DB_NAME="fleet_flow_db"
-MODULE_TO_UPDATE="fleet_flow"
+# ENTERPRISE_ADDONS="/home/bhakimi/workspace/source/enterprise/v 16.0/odoo/addons"
+# CUSTOM_ADDONS="/home/bhakimi/workspace/source/community/v 16.0/my_modules,/home/bhakimi/workspace/source/community/v 16.0/myaddons"
+CUSTOM_ADDONS="/home/bhakimi/workspace/source/community/v 16.0/odoo-IFRC-addon"
+DB_NAME="community_db"
+MODULE_TO_UPDATE="volunteer_management"
 
 # Start Odoo with configurable options
 source venv/bin/activate
-python3 "$ODOO_BIN" --addons-path="$ADDONS_PATH,$ENTERPRISE_ADDONS,$CUSTOM_ADDONS" -d "$DB_NAME" -u $MODULE_TO_UPDATE --dev xml
+python3 "$ODOO_BIN" --addons-path="$ADDONS_PATH,$CUSTOM_ADDONS" --xmlrpc-port=8069 --dev xml -u "$MODULE_TO_UPDATE"
